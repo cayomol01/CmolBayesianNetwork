31d56fe418d88312855c9b2fd997d1634a96e5e8
cayomol01 <jacmg01@gmail.com>
2023-02-16 19:37:08 -0600

    compact

--- CmolBayesianNetwork/main.py
+++ CmolBayesianNetwork/main.py
@@ -1,11 +1,11 @@
 from Network import Network
 
 
-nodos = ['A', 'B', 'C', 'E', 'F', 'G', 'H']
+nodos = ['A', 'B', 'C', 'D', 'E']
 bayes = Network(nodos)
 bayes.addParents('C', ['A', 'B'])
+bayes.addParents('D', ['C'])
 bayes.addParents('E', ['C'])
-bayes.addParents('F', ['C'])
-bayes.addParents('G', ['F'])
-bayes.addParents('H', ['F', 'E', 'G'])
+
 print(bayes.formaCompacta())
+
